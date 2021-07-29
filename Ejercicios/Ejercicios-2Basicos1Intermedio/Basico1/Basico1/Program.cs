05d359a420530f8c276e2f4bed505c14f8a26b6f
CarloDiazP <88125777+CarloDiazP@users.noreply.github.com>
2021-07-29 18:29:40 -0500

    Add files via upload

---
+++ Ejercicios/Ejercicios-2Basicos1Intermedio/Basico1/Basico1/Program.cs
@@ -0,0 +1,23 @@
+﻿//Autor: Carlo Rodrigo Diaz Portilla
+using System;
+
+namespace Basico1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.Write("Ingrese el número a evaluar: ");
+            num = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= (num / 2); i++)
+            {
+                if (num % i == 0)
+                    Console.Write(i + ", ");
+            }
+            Console.Write(num);
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Ejercicios/Ejercicios-2Basicos1Intermedio/Medio1/Medio1/Program.cs
@@ -0,0 +1,25 @@
+﻿//Autor: Carlo Rodrigo Diaz Portilla
+using System;
+
+namespace Medio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, numFibonacci;
+            Console.Write("Ingrese el número para obtener Fibonacci: ");
+            num = int.Parse(Console.ReadLine());
+            numFibonacci = fibonacci(num);
+            Console.WriteLine("El número Fibonacci de " + num + " es: " + numFibonacci);
+            Console.ReadKey();
+        }
+        static int fibonacci(int num)
+        {
+            if (num == 1 || num == 0)
+                return num;
+            else
+                return fibonacci(num - 1) + fibonacci(num - 2);
+        }
+    }
+}
