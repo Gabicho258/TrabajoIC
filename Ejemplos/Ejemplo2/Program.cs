ef595f608707f38fc76c4152ca972396839b54f7
fvilcaqui <87882802+fvilcaqui@users.noreply.github.com>
2021-07-29 19:05:20 -0500

    Add files via upload

---
+++ Ejemplos/Ejemplo2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ejemplo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables y tipos de datos
+            string nombre = "Javier";
+            int numero = 1;
+            bool respuesta = true;
+            double talla = 1.80;
+            char b = default;
+            char abecedario = b;
+            //La variable var es muy flexible por lo que 
+            //puede tomar el valor de otras variables
+            var num = 23; //entero
+            var palabra = "cocina"; //string
+            var peso = 10.8; //decimal
+            var amor = true; //boolean
+
+        }
+    }
+}
---
+++ Ejemplos/Ejemplo3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ejemplo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aqui le daremos los valores a los numeros
+            int num1 = 15, num2 = 50;
+            //veremos que numero es mayor para que se cumpla la condicion
+            if (num1 >= num2)
+            {
+                Console.WriteLine("El primer numero es mayor");
+            }
+            else
+            {
+                Console.WriteLine("El segundo numero es mayor");
+            }
+            Console.ReadKey();
+        }
+    }
+}
